cc8398aa05d689d11ba21447105509b55e26b81b
wz36125 <717179175@qq.com>
2020-05-06 15:11:57 +0800

    主

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    title: '个人文档',
+    description: '练习文档',
+    themeConfig: {
+        logo: '/img/logo.png',
+        sidebar: {
+            '/test/' : [
+                ['/test/aa.md','测试1'],
+                ['/test/bb.md','测试2']
+
+            ]
+        }
+    }
+}
